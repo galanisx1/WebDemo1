f2ee1f24f267517031aea4ad60676e135b472974
galanisx1 <100341218+galanisx1@users.noreply.github.com>
2024-12-04 14:20:03 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,4 +7,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Asegúrate de que Flask escuche en el puerto 5000 en la dirección 0.0.0.0
+    app.run(host="0.0.0.0", port=5000)
